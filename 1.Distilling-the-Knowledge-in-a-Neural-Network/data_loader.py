7a7d5450a4a38bde564985baec90ad43957a42a4
corner <corner@88.com>
2023-08-25 13:30:16 +0800

    add-gitignore

---
+++ 1.Distilling-the-Knowledge-in-a-Neural-Network/data_loader.py
@@ -0,0 +1,35 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.utils.data as data
+
+
+def load_data_MNIST(batch_size, resize=None):
+    """Download the MNIST dataset and then load it into memory."""
+    trans = [transforms.ToTensor()]
+
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+
+    mnist_train = torchvision.datasets.MNIST(root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.MNIST(root="../data", train=False, transform=trans, download=False)
+
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,num_workers=4),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,num_workers=4))
+
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """Download the Fashion-MNIST dataset and then load it into memory."""
+    trans = [transforms.ToTensor()]
+
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+
+    mnist_train = torchvision.datasets.FashionMNIST(root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root="../data", train=False, transform=trans, download=True)
+
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,num_workers=4),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,num_workers=4))
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### 知识蒸馏（Knowledge Distillation）
 
-1. basic knowledge: [Distilling the Knowledge in a Neural Network](1.Distilling%20the%20Knowledge%20in%20a%20Neural%20Network/index.md)
+1. Basic knowledge: [Distilling the Knowledge in a Neural Network](1.Distilling-the-Knowledge-in-a-Neural-Network/index.md)
 
